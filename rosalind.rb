8791f1a5ca00b807937651a5dae2c10216c0539e
Alpha Chen <alpha.chen@gmail.com>
2014-10-11 13:50:01 -0700

    Make running datasets easier

--- rosalind.rb
+++ rosalind.rb
@@ -1,9 +1,34 @@
 require_relative 'dna'
 require_relative 'misc'
 
+module Rosalind
+  class Base
+    attr_reader :input
+
+    def initialize(input)
+      @input = input.strip
+    end
+
+    def run
+      raise NotImplementedError
+    end
+  end
+
+  class LEXV < Base
+    def run
+      alphabet, n = input.split("\n")
+      alphabet = alphabet.split(/\s+/)
+      n = n.to_i
+
+      puts lexv(alphabet, n)
+    end
+  end
+end
+
 if __FILE__ == $0
-  require 'pry'
-  require 'pry-byebug'
-  binding.pry
-  exit # For Pry
+  file = ARGV.shift
+  problem = file[/rosalind_(\w+).txt/, 1]
+  input = File.read(file)
+
+  Rosalind.const_get(problem.upcase).new(input).run
 end

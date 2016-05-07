5e93a2a5a6b6b7cc79abd14f6b4992bf0c35f94c
Alpha Chen <alpha.chen@gmail.com>
2016-05-07 07:59:09 -0700

    [rust][anagram] Solution

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,15 +1,38 @@
-pub fn anagrams_for<'a>(word: &str, candidates: &'a [&str]) -> Vec<&'a str> {
-    let fingerprint = fingerprint(word);
-
-    println!("{:?}",
-             candidates.iter()
-                       .filter(|x| fingerprint(x) == fingerprint)
-                       .collect());
-    vec![]
+#[derive(Debug)]
+struct Anagram {
+    word: String,
 }
 
-fn fingerprint(s: &str) -> Vec<char> {
-    let mut fingerprint: Vec<_> = s.chars().collect();
-    fingerprint.sort();
-    fingerprint
+impl Anagram {
+    fn new(s: &str) -> Anagram {
+        Anagram { word: s.into() }
+    }
+
+    fn sorted_chars(s: &str) -> Vec<char> {
+        let mut sorted_chars: Vec<_> = s.to_lowercase().chars().collect();
+        sorted_chars.sort();
+        sorted_chars
+    }
+}
+
+impl PartialEq for Anagram {
+    fn eq(&self, other: &Anagram) -> bool {
+        self.word.to_lowercase() != other.word.to_lowercase() &&
+        Self::sorted_chars(&self.word) == Self::sorted_chars(&other.word)
+    }
+}
+
+#[test]
+fn anagram_equality() {
+    assert!(Anagram::new("") != Anagram::new(""));
+    assert_eq!(Anagram::new("omg"), Anagram::new("mog"));
+}
+
+pub fn anagrams_for<'a>(word: &str, candidates: &[&'a str]) -> Vec<&'a str> {
+    let anagram = Anagram::new(word);
+
+    candidates.iter()
+              .map(|&x| x)
+              .filter(|&x| Anagram { word: x.into() } == anagram)
+              .collect::<Vec<_>>()
 }

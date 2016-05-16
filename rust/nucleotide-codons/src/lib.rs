aea754d4aa339e13b322e62f7eafc3fef2675d9b
Alpha Chen <alpha.chen@gmail.com>
2016-05-16 08:45:36 -0700

    [rust][nucleotide-codons]

--- rust/nucleotide-codons/src/lib.rs
+++ rust/nucleotide-codons/src/lib.rs
@@ -1,19 +1,49 @@
-use std::collections::HashMap;
-
-pub fn parse(pairs: Vec<(&str, &str)>) -> Codons {
-    Codons { pairs: pairs.iter().map(|&(c, n)| (c.into(), n.into())).collect() }
+pub fn parse<'a>(pairs: Vec<(&'a str, &'a str)>) -> Codons<'a> {
+    Codons { pairs: pairs.iter().map(|x| Codon::new(x)).collect() }
 }
 
-pub struct Codons {
-    pairs: HashMap<String, String>,
+pub struct Codons<'a> {
+    pairs: Vec<Codon<'a>>,
 }
 
-impl Codons {
+impl<'a> Codons<'a> {
     pub fn name_for(&self, shorthand: &str) -> Result<&str, ()> {
+        if shorthand.is_empty() {
+            return Err(());
+        }
+
         self.pairs
             .iter()
-            .find(|&(c, _)| c == shorthand)
-            .map(|(_, n)| n)
+            .find(|ref x| x.matches(shorthand))
+            .map(|ref x| x.name)
             .ok_or(())
+    }
+}
+
+struct Codon<'a> {
+    codon: &'a str,
+    name: &'a str,
+}
+
+impl<'a> Codon<'a> {
+    fn new(raw: &(&'a str, &'a str)) -> Codon<'a> {
+        Codon {
+            codon: raw.0,
+            name: raw.1,
+        }
+    }
+
+    fn matches(&self, shorthand: &str) -> bool {
+        shorthand.chars().zip(self.codon.chars()).all(|(a, b)| {
+            match a {
+                'H' => vec!['A', 'C', 'T'],
+                'M' => vec!['A', 'C'],
+                'N' => vec!['A', 'C', 'G', 'T'],
+                'R' => vec!['A', 'G'],
+                'Y' => vec!['C', 'T'],
+                _ => vec![a],
+            }
+            .contains(&b)
+        })
     }
 }

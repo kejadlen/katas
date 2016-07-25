e4be4422f4d940356404831ae0c7fe07dda34891
Alpha Chen <alpha.chen@gmail.com>
2016-07-24 19:26:25 -0700

    another verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,8 @@
 class Bottles
   def verse(n)
     <<-VERSE
-99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
+#{n} bottles of beer on the wall, #{n} bottles of beer.
+Take one down and pass it around, #{n-1} bottles of beer on the wall.
     VERSE
   end
 end

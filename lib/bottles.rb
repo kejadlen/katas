5c84f436e7d644cb4de783b958eebd051b9e9fb8
Alpha Chen <alpha.chen@gmail.com>
2016-07-24 19:32:56 -0700

    verse 1

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,8 +2,16 @@ class Bottles
   def verse(n)
     <<-VERSE
 #{bottle_plural(n)} of beer on the wall, #{bottle_plural(n)} of beer.
-Take one down and pass it around, #{bottle_plural(n-1)} of beer on the wall.
+#{second_line(n)}
     VERSE
+  end
+
+  def second_line(n)
+    if n == 1
+      'Take it down and pass it around, no more bottles of beer on the wall.'
+    else
+      "Take one down and pass it around, #{bottle_plural(n-1)} of beer on the wall."
+    end
   end
 
   def bottle_plural(n)

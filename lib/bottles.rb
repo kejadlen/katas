11d868184a5a2fff8114e88832d7546d08e273b4
Alpha Chen <alpha.chen@gmail.com>
2016-07-24 19:38:15 -0700

    verse 0

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,20 +1,27 @@
 class Bottles
   def verse(n)
     <<-VERSE
-#{bottle_plural(n)} of beer on the wall, #{bottle_plural(n)} of beer.
-#{second_line(n)}
+#{bottle_plural(n).capitalize} of beer on the wall, #{bottle_plural(n)} of beer.
+#{command(n)}, #{bottle_plural(next_quantity(n))} of beer on the wall.
     VERSE
   end
 
-  def second_line(n)
-    if n == 1
-      'Take it down and pass it around, no more bottles of beer on the wall.'
+  def command(n)
+    case n
+    when 1
+      'Take it down and pass it around'
+    when 0
+      'Go to the store and buy some more'
     else
-      "Take one down and pass it around, #{bottle_plural(n-1)} of beer on the wall."
+      'Take one down and pass it around'
     end
   end
 
+  def next_quantity(n)
+    (n == 0) ? 99 : n - 1
+  end
+
   def bottle_plural(n)
-    "#{n} bottle#{?s if n > 1}"
+    "#{(n == 0) ? 'no more': n} bottle#{?s unless n == 1}"
   end
 end

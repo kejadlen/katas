954b3f77be69feab7788333ca6ebcfd5a63f940b
Alpha Chen <alpha.chen@gmail.com>
2016-07-26 07:55:16 -0700

    simplify

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,92 +8,87 @@ class Bottles
   end
 
   def verse(n)
-    Verse.new(n).to_s
+    Verse[n].to_s
   end
 
-  class Verse < SimpleDelegator
-    def initialize(n)
-      case n
-      when 0
-        super(Zero.new(n))
-      when 1
-        super(One.new(n))
-      when 6
-        super(Six.new(n))
-      else
-        super(Base.new(n))
-      end
+  class Verse
+    def self.[](n)
+      klass = "::#{self.name.split('::').first}::Verse#{n}"
+      klass = const_defined?(klass) ? const_get(klass) : self
+      klass.new(n)
     end
 
-    class Base
-      attr_reader :n
+    attr_reader :n
 
-      def initialize(n)
-        @n = n
-      end
+    def initialize(n)
+      @n = n
+    end
 
-      def to_s
-        <<-VERSE
+    def to_s
+      <<-VERSE
 #{refrain.capitalize}, #{descriptor}.
 #{command}, #{succ.refrain}.
-        VERSE
-      end
-
-      def refrain
-        "#{descriptor} on the wall"
-      end
-
-      def descriptor
-        "#{uber_quantity} of beer"
-      end
-
-      def uber_quantity
-        "#{quantity} bottle#{?s if plural?}"
-      end
-
-      private
-
-      def command
-        'Take one down and pass it around'
-      end
-
-      def quantity
-        n
-      end
-
-      def plural?
-        n != 1
-      end
-
-      def succ
-        Verse.new(n - 1)
-      end
+      VERSE
     end
 
-    class Six < Base
-      def uber_quantity
-        '1 six-pack'
-      end
+    def refrain
+      "#{descriptor} on the wall"
     end
 
-    class One < Base
-      def command
-        'Take it down and pass it around'
-      end
+    def descriptor
+      "#{uber_quantity} of beer"
     end
 
-    class Zero < Base
-      def command
-        'Go to the store and buy some more'
-      end
+    def uber_quantity
+      "#{quantity} bottle#{?s if plural?}"
+    end
 
-      def quantity
-        'no more'
-      end
+    private
 
-      def succ
-        Verse.new(99)
-      end
+    def command
+      'Take one down and pass it around'
+    end
+
+    def quantity
+      n
+    end
+
+    def plural?
+      n != 1
+    end
+
+    def succ
+      Verse[next_verse]
+    end
+
+    def next_verse
+      n - 1
+    end
+  end
+
+  class Verse6 < Verse
+    def uber_quantity
+      '1 six-pack'
+    end
+  end
+
+  class Verse1 < Verse
+    def command
+      'Take it down and pass it around'
+    end
+  end
+
+  class Verse0 < Verse
+    def command
+      'Go to the store and buy some more'
+    end
+
+    def quantity
+      'no more'
+    end
+
+    def next_verse
+      99
     end
   end
 end

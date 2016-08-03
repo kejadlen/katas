f1ee463103b6053ee95efa42e180fd591c33ae3d
Alpha Chen <alpha.chen@gmail.com>
2016-08-02 20:08:26 -0700

    [rust][difference-of-squares] Use Add::add instead of a block

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,11 +1,13 @@
 #![feature(inclusive_range_syntax)]
 
+use std::ops::Add;
+
 pub fn difference(i: usize) -> usize {
     square_of_sum(i) - sum_of_squares(i)
 }
 
 pub fn square_of_sum(i: usize) -> usize {
-    (1...i).fold(0, |acc, x| acc + x).pow(2)
+    (1...i).fold(0, Add::add).pow(2)
 }
 
 pub fn sum_of_squares(i: usize) -> usize {

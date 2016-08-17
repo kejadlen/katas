0d509aab584fe4d9835c2e092e5729200517d56d
Alpha Chen <alpha.chen@gmail.com>
2016-08-17 08:37:26 -0700

    [rust][word-count] Use Split

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -2,32 +2,21 @@ use std::collections::HashMap;
 
 pub fn word_count(s: &str) -> HashMap<String, u32> {
     let mut result = HashMap::new();
-    for word in String::from(s).alphanumeric_tokens() {
+    for word in String::from(s)
+                    .split(char::is_not_alphanumeric)
+                    .filter(|x| !x.is_empty()) {
         let count = result.entry(word.to_lowercase()).or_insert(0);
         *count += 1;
     }
     result
 }
 
-trait AlphaNumericSplitter {
-    fn alphanumeric_tokens(&self) -> Vec<String>;
+trait WordCount {
+    fn is_not_alphanumeric(self) -> bool;
 }
 
-impl AlphaNumericSplitter for String {
-    fn alphanumeric_tokens(&self) -> Vec<String> {
-        let mut result = Vec::new();
-        let mut current = String::new();
-        for c in self.chars() {
-            if c.is_alphanumeric() {
-                current.push(c);
-            } else if !current.is_empty() {
-                result.push(current);
-                current = String::new();
-            }
-        }
-        if !current.is_empty() {
-            result.push(current);
-        }
-        result
+impl WordCount for char {
+    fn is_not_alphanumeric(self) -> bool {
+        !self.is_alphanumeric()
     }
 }

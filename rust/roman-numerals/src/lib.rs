b4d2839b6ca1786bb034273d88d049e0072764df
Alpha Chen <alpha.chen@gmail.com>
2016-09-19 15:36:50 -0700

    [rust][roman-numerals] II

--- rust/roman-numerals/src/lib.rs
+++ rust/roman-numerals/src/lib.rs
@@ -1,15 +1,15 @@
 pub struct Roman {
-    n: usize,
+  n: usize,
 }
 
 impl From<usize> for Roman {
-    fn from(n: usize) -> Roman {
-        Roman { n: n }
-    }
+  fn from(n: usize) -> Roman {
+    Roman { n: n }
+  }
 }
 
 impl ToString for Roman {
-    fn to_string(&self) -> String {
-        "I".into()
-    }
+  fn to_string(&self) -> String {
+    std::iter::repeat('I').take(self.n).collect::<String>()
+  }
 }

286bd493f6a72df0cb3c23366a1eface0bc336a5
Alpha Chen <alpha.chen@gmail.com>
2016-09-19 21:34:02 -0700

    [rust][raindrops] PlingPlangPlong

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -9,6 +9,10 @@ pub fn raindrops(n: usize) -> String {
     result.push_str("Plang");
   }
 
+  if n % 7 == 0 {
+    result.push_str("Plong");
+  }
+
   if result.is_empty() {
     result = n.to_string()
   }

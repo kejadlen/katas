0282895d5c92452c29c1f560d0a6c2194e28c3cc
Alpha Chen <alpha.chen@gmail.com>
2016-09-20 08:22:25 -0700

    [rust][beer-song] 0th verse

---
+++ rust/beer-song/src/lib.rs
@@ -0,0 +1,10 @@
+pub fn sing(start: usize, end: usize) -> String {
+  "".into()
+}
+
+pub fn verse(bottles: usize) -> String {
+  "No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+"
+    .into()
+}

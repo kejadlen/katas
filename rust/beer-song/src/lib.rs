aa2f5b46040cd476ee43cd1fb93a3e8b27f4164a
Alpha Chen <alpha.chen@gmail.com>
2016-09-20 08:23:49 -0700

    [rust][beer-song] verse 1

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -3,8 +3,14 @@ pub fn sing(start: usize, end: usize) -> String {
 }
 
 pub fn verse(bottles: usize) -> String {
-  "No more bottles of beer on the wall, no more bottles of beer.
+  if bottles > 0 {
+      "1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+"
+    } else {
+      "No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
 "
+    }
     .into()
 }

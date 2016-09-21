ee2f7c02ca3c1ab75f3216a1fddade4065bbc48c
Alpha Chen <alpha.chen@gmail.com>
2016-09-21 10:15:27 -0700

    [rust][pascals-triangle] one_row

--- rust/pascals-triangle/src/lib.rs
+++ rust/pascals-triangle/src/lib.rs
@@ -1,11 +1,17 @@
-pub struct PascalsTriangle;
+pub struct PascalsTriangle {
+  row: u32,
+}
 
 impl PascalsTriangle {
-  pub fn new(row_count: u32) -> Self {
-    PascalsTriangle {}
+  pub fn new(row: u32) -> Self {
+    PascalsTriangle { row: row }
   }
 
   pub fn rows(&self) -> Vec<Vec<u32>> {
-    Vec::new()
+    match self.row {
+      0 => Vec::new(),
+      1 => vec![vec![1]],
+      _ => Vec::new(),
+    }
   }
 }

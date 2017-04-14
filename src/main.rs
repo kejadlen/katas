1301c652484f408ac84786e3d8ea95383fded748
Alpha Chen <alpha.chen@gmail.com>
2017-04-14 13:02:12 -0700

    disable raw mode automatically

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,32 @@ use std::io::prelude::*;
 use std::mem;
 
 fn main() {
-    enable_raw_mode();
-
     println!("Hello, world!");
-
-    let stdin = io::stdin();
-    let chars = stdin.chars().flat_map(|x| x).take_while(|c| *c != 'q');
-    for c in chars {
-        println!("{}", c);
-    }
+    setup_terminal(|| {
+                       let stdin = io::stdin();
+                       let chars = stdin.chars().flat_map(|x| x).take_while(|c| *c != 'q');
+                       for c in chars {
+                           println!("{}", c);
+                       }
+                   });
 }
 
-fn enable_raw_mode() {
+fn setup_terminal<F>(f: F)
+    where F: Fn() -> ()
+{
+    let mut original_termios: libc::termios;
     unsafe {
-        let mut raw: libc::termios = mem::zeroed();
-        libc::tcgetattr(libc::STDIN_FILENO, &mut raw);
+        original_termios = mem::zeroed();
+        libc::tcgetattr(libc::STDIN_FILENO, &mut original_termios);
+
+        let mut raw = original_termios.clone();
         raw.c_lflag &= !libc::ECHO;
         libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &raw);
+    }
+
+    f();
+
+    unsafe {
+        libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &original_termios);
     }
 }

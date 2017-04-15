34000d3b438f616ab29cd1c8112bd3c2bf8c72f1
Alpha Chen <alpha.chen@gmail.com>
2017-04-14 17:53:23 -0700

    use an iterator to get keystrokes

--- src/main.rs
+++ src/main.rs
@@ -6,46 +6,61 @@ use std::mem;
 
 fn main() {
     println!("Hello, world!");
-    setup_terminal(|| {
-        let mut stdin = io::stdin();
-        let mut buf: [u8; 1] = [0];
-        loop {
-            stdin.read_exact(&mut buf);
-            let c = buf[0] as char;
-            if c.is_control() {
-                println!("{}\r", c as u8);
-            } else {
-                println!("{} {}\r", c as u8, c);
-            }
-            if c == 'q' {
-                return;
-            }
+    let keystrokes = Keystrokes::new();
+    for c in keystrokes.take_while(|c| *c != 'q') {
+        if c.is_control() {
+            println!("{}\r", c as u8);
+        } else {
+            println!("{} {}\r", c as u8, c);
         }
-    });
+    }
 }
 
-fn setup_terminal<F>(f: F)
-    where F: Fn() -> ()
-{
-    let mut original_termios: libc::termios;
-    unsafe {
-        original_termios = mem::zeroed();
-        libc::tcgetattr(libc::STDIN_FILENO, &mut original_termios);
+struct Keystrokes {
+    stdin: io::Stdin,
+    termios: libc::termios,
+}
 
-        let mut raw = original_termios;
-        raw.c_iflag &= !(libc::BRKINT | libc::ICRNL | libc::INPCK | libc::ISTRIP | libc::IXON);
-        raw.c_oflag &= !libc::OPOST;
-        raw.c_cflag |= libc::CS8;
-        raw.c_lflag &= !(libc::ECHO | libc::ICANON | libc::IEXTEN | libc::ISIG);
-        raw.c_cc[libc::VMIN] = 0;
-        raw.c_cc[libc::VTIME] = 1;
+impl Keystrokes {
+    fn new() -> Self {
+        let mut termios;
+        unsafe {
+            termios = mem::zeroed();
+            libc::tcgetattr(libc::STDIN_FILENO, &mut termios);
 
-        libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &raw);
+            let mut raw = termios;
+            raw.c_iflag &= !(libc::BRKINT | libc::ICRNL | libc::INPCK | libc::ISTRIP | libc::IXON);
+            raw.c_oflag &= !libc::OPOST;
+            raw.c_cflag |= libc::CS8;
+            raw.c_lflag &= !(libc::ECHO | libc::ICANON | libc::IEXTEN | libc::ISIG);
+            raw.c_cc[libc::VMIN] = 0;
+            raw.c_cc[libc::VTIME] = 1;
+
+            libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &raw);
+        }
+
+        let stdin = io::stdin();
+        Self { stdin, termios }
     }
+}
 
-    f();
+impl Drop for Keystrokes {
+    fn drop(&mut self) {
+        unsafe {
+            libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &self.termios);
+        }
+    }
+}
 
-    unsafe {
-        libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &original_termios);
+impl Iterator for Keystrokes {
+    type Item = char;
+
+    fn next(&mut self) -> Option<char> {
+        let mut buf: [u8; 1] = [0];
+        loop {
+            if self.stdin.read_exact(&mut buf).is_ok() {
+                return Some(buf[0] as char);
+            }
+        }
     }
 }

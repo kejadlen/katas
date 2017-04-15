0c0a441982c006afe97b2b75a479e6a5e055460e
Alpha Chen <alpha.chen@gmail.com>
2017-04-15 09:03:19 -0700

    split out code into lib

---
+++ src/lib.rs
@@ -0,0 +1,74 @@
+#![recursion_limit = "1024"]
+
+#[macro_use]
+extern crate error_chain;
+extern crate libc;
+
+pub mod errors {
+    error_chain! {
+        foreign_links {
+            Io(::std::io::Error);
+        }
+    }
+}
+
+use std::io;
+use std::io::prelude::*;
+use std::mem;
+
+use errors::*;
+
+pub struct Keystrokes {
+    stdin: io::Stdin,
+    termios: libc::termios,
+}
+
+impl Keystrokes {
+    pub fn new() -> Result<Self> {
+        let mut termios;
+        unsafe {
+            termios = mem::zeroed();
+            if libc::tcgetattr(libc::STDIN_FILENO, &mut termios) == -1 {
+                bail!(Error::with_chain(io::Error::last_os_error(), "tcgetattr"));
+            }
+
+            let mut raw = termios;
+            raw.c_iflag &= !(libc::BRKINT | libc::ICRNL | libc::INPCK | libc::ISTRIP | libc::IXON);
+            raw.c_oflag &= !libc::OPOST;
+            raw.c_cflag |= libc::CS8;
+            raw.c_lflag &= !(libc::ECHO | libc::ICANON | libc::IEXTEN | libc::ISIG);
+            raw.c_cc[libc::VMIN] = 0;
+            raw.c_cc[libc::VTIME] = 1;
+
+            if libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &raw) == -1 {
+                bail!(Error::with_chain(io::Error::last_os_error(), "tcsetattr"));
+            }
+        }
+
+        let stdin = io::stdin();
+        Ok(Self { stdin, termios })
+    }
+}
+
+impl Drop for Keystrokes {
+    fn drop(&mut self) {
+        unsafe {
+            libc::tcsetattr(libc::STDIN_FILENO, libc::TCSAFLUSH, &self.termios);
+        }
+    }
+}
+
+impl Iterator for Keystrokes {
+    type Item = char;
+
+    fn next(&mut self) -> Option<char> {
+        let mut buf: [u8; 1] = [0];
+        loop {
+            match self.stdin.read_exact(&mut buf) {
+                Ok(_) => return Some(buf[0] as char),
+                Err(ref e) if e.kind() != io::ErrorKind::UnexpectedEof => return None,
+                _ => {}
+            }
+        }
+    }
+}

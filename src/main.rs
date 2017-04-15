659dec5ed37dc0d97523bcdabdd905ba6340f4a1
Alpha Chen <alpha.chen@gmail.com>
2017-04-15 14:32:55 -0700

    clear screen before/after running, remove debug printing

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,39 @@ use kilo::*;
 use kilo::errors::*;
 
 quick_main!(|| -> Result<()> {
-    println!("Hello, world!");
-    let key_presses = KeyPresses::new()?;
-    for c in key_presses {
-        refresh_screen();
-        match c {
-            c if c == ctrl_key('q') => { return Ok(()) },
-            c => {
-                if c.is_control() {
-                    println!("{}\r", c as u8);
-                } else {
-                    println!("{} {}\r", c as u8, c);
-                }
+    run(|| {
+        let key_presses = KeyPresses::new()?;
+        for c in key_presses {
+            refresh_screen();
+            match c {
+                c if c == ctrl_key('q') => return Ok(()),
+                c => {}
             }
         }
-    }
 
-    Ok(())
+        Ok(())
+    })
 });
 
-fn ctrl_key(c: char) -> char {
-    ((c as u8) & 0x1f) as char
+fn run<F>(f: F) -> Result<()>
+    where F: Fn() -> Result<()>
+{
+    clear_screen();
+    let result = f();
+    clear_screen();
+    result
+}
+
+
+fn clear_screen() {
+    println!("\x1b[2J");
+    println!("\x1b[H");
 }
 
 fn refresh_screen() {
-    println!("\x1b[2J");
-    println!("\x1b[H");
+    clear_screen();
+}
+
+fn ctrl_key(c: char) -> char {
+    ((c as u8) & 0x1f) as char
 }

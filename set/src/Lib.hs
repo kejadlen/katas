d46930d41e5d3b272899fab273dd7f24205155a3
Alpha Chen <alpha.chen@gmail.com>
2017-05-31 08:07:21 -0700

    HTF setup

--- set/src/Lib.hs
+++ set/src/Lib.hs
@@ -1,6 +1,8 @@
 module Lib
-    ( someFunc
+    ( myReverse
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+myReverse :: [a] -> [a]
+myReverse []     = []
+myReverse [x]    = [x]
+myReverse (x:xs) = myReverse xs
--- set/test/Spec.hs
+++ set/test/Spec.hs
@@ -1,2 +1,14 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
+
+import Test.Framework
+import Lib
+
+test_nonEmpty = do assertEqual [1] (myReverse [1])
+                   assertEqual [3,2,1] (myReverse [1,2,3])
+
+test_empty = assertEqual ([] :: [Int]) (myReverse [])
+
+prop_reverse :: [Int] -> Bool
+prop_reverse xs = xs == (myReverse (myReverse xs))
+
+main = htfMain htf_thisModulesTests

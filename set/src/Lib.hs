cd4106be6997e9a9d53a60b7b9ea340d6e58df9c
Alpha Chen <alpha.chen@gmail.com>
2017-06-01 07:57:45 -0700

    uniqueness

--- set/src/Lib.hs
+++ set/src/Lib.hs
@@ -4,20 +4,32 @@ module Lib
     , isEmpty
     , add
     , length
+    , contains
     ) where
 
 import Prelude hiding (length)
 
-data Set = Set Int
+data Set = Empty | Cons Int Set
 
 empty :: Set
-empty = Set 0
+empty = Empty
 
 isEmpty :: Set -> Bool
-isEmpty (Set n) = n == 0
+isEmpty Empty = True
+isEmpty _ = False
 
 add :: Int -> Set -> Set
-add _ (Set n) = Set (n + 1)
+add x Empty = Cons x Empty
+add e (Cons x xs)
+  | e == x    = Cons x xs
+  | otherwise = Cons x (add e xs)
 
 length :: Set -> Int
-length (Set n) = n
+length Empty = 0
+length (Cons _ s) = 1 + (length s)
+
+contains :: Int -> Set -> Bool
+contains _ Empty = False
+contains e (Cons x s)
+  | e == x    = True
+  | otherwise = contains e s
--- set/test/Spec.hs
+++ set/test/Spec.hs
@@ -9,6 +9,12 @@ test_isEmpty = do assertEqual True (isEmpty empty)
 
 test_length = do assertEqual 0 (length empty)
                  assertEqual 1 (length (add 1 empty))
-                 assertEqual 2 (length (add 1 (add 1 empty)))
+                 assertEqual 2 (length (add 2 (add 1 empty)))
+
+test_contains = do assertEqual False (contains 0 empty)
+                   assertEqual False (contains 0 (add 1 empty))
+                   assertEqual True (contains 1 (add 1 empty))
+
+test_uniqueness = do assertEqual 1 (length (add 1 (add 1 empty)))
 
 main = htfMain htf_thisModulesTests

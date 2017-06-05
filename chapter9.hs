0d5de8cf8af43df4bc36e96ad3322c90d8a11899
Alpha Chen <alpha.chen@gmail.com>
2017-06-05 07:55:54 -0700

    moar chapter 9, including cipher

--- chapter9.hs
+++ chapter9.hs
@@ -1,3 +1,6 @@
+import Data.Bool
+import Data.Char
+
 eftBool :: Bool -> Bool -> [Bool]
 eftBool False False = [False]
 eftBool False True = [False, True]
@@ -45,3 +48,25 @@ myCube = [y^3 | y <- [1..5]]
 -- [(x,y) | x <- mySqr, y <- myCube]
 -- [(x,y) | x <- mySqr, y <- myCube, x < 50, y < 50]
 -- length [(x,y) | x <- mySqr, y <- myCube, x < 50, y < 50]
+
+negThree = map (\x -> bool x (-x) (x == 3))
+
+filtMult3 = filter (\x -> mod x 3 == 0)
+
+myFilter :: [Char] -> [[Char]]
+myFilter sentence = filter (\x -> not $ elem x ["the", "a", "an"]) (myWords sentence)
+
+myZip :: [a] -> [b] -> [(a,b)]
+-- myZip _ [] = []
+-- myZip [] _ = []
+-- myZip (a:as) (b:bs) = (a,b) : myZip as bs
+myZip = myZipWith (\a -> \b -> (a,b))
+
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith f a b = map (\(x,y) -> f x y) $ zip a b
+
+filterUpper = filter isUpper
+capitalize (c:cs) = toUpper c : cs
+capitalize' "woot" = "WOOT"
+capitalize' (c:cs) = toUpper c : cs
+capitalizeFirst = toUpper . head

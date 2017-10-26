18918f28ba2c0b13d52fd0a9f80b67593ec53cf4
Alpha Chen <alpha.chen@gmail.com>
2017-10-26 17:45:05 -0500

    unit test jsonic

--- jsonic/tokenizer.rkt
+++ jsonic/tokenizer.rkt
@@ -1,8 +1,17 @@
 #lang br/quicklang
 (require brag/support racket/contract)
 
+(module+ test
+  (require rackunit))
+
 (define (jsonic-token? x)
   (or (eof-object? x) (string? x) (token-struct? x)))
+
+(module+ test
+  (check-true (jsonic-token? eof))
+  (check-true (jsonic-token? "a string"))
+  (check-true (jsonic-token? (token 'A-TOKEN-STRUCT "hi")))
+  (check-false (jsonic-token? 42)))
 
 (define (make-tokenizer port)
   (define (next-token)
@@ -18,3 +27,15 @@
 (provide
   (contract-out
     [make-tokenizer (input-port? . -> . (-> jsonic-token?))]))
+
+(module+ test
+  (check-equal?
+   (apply-tokenizer-maker make-tokenizer "// comment\n")
+   empty)
+  (check-equal?
+   (apply-tokenizer-maker make-tokenizer "@$ (+ 6 7) $@")
+   (list (token-struct 'SEXP-TOK " (+ 6 7) " #f #f #f #f #f)))
+  (check-equal?
+   (apply-tokenizer-maker make-tokenizer "hi")
+   (list (token-struct 'CHAR-TOK "h" #f #f #f #f #f)
+         (token-struct 'CHAR-TOK "i" #f #f #f #f #f))))

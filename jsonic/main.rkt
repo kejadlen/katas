054f5b0469aa579744a2de50c5d5f9cbdd2996c3
Alpha Chen <alpha.chen@gmail.com>
2017-10-26 22:42:27 -0500

    jsonic indenting and toolbar button

--- jsonic/main.rkt
+++ jsonic/main.rkt
@@ -8,9 +8,9 @@
       (case key
         [(color-lexer)
          (dynamic-require 'jsonic/colorer 'color-jsonic)]
-        #| [(drracket:indentation) |#
-        #|  (dynamic-require 'jsonic/indenter 'indent-jsonic)] |#
-        #| [(drracket:toolbar-buttons) |#
-        #|  (dynamic-require 'jsonic/buttons 'button-list)] |#
+        [(drracket:indentation)
+         (dynamic-require 'jsonic/indenter 'indent-jsonic)]
+        [(drracket:toolbar-buttons)
+         (dynamic-require 'jsonic/buttons 'button-list)]
         [else default]))
     handle-query))
